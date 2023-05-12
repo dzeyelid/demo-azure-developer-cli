21cdf2d9c05614964daea2f6331200ca93d08c5c
Kazumi IWANAGA(OHIRA) / @dz_ <dzeyelid@gmail.com>
2023-05-12 17:34:18 +0000

    Update api

--- src/api/hello/index.ts
+++ src/api/hello/index.ts
@@ -4,7 +4,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? "Hooray, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
